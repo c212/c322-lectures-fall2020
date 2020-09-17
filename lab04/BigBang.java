095cb918c9b8a026b2e92e078a91c8aea86c70ad
c212 <40395648+c212@users.noreply.github.com>
2020-09-17 16:37:32 -0400

    adding a timer
    the big bang is now also an actionlistener

--- lab04/BigBang.java
+++ lab04/BigBang.java
@@ -2,17 +2,27 @@ import java.awt.Graphics;
 import java.awt.Container; 
 import javax.swing.JComponent; 
 import javax.swing.JFrame; 
+import java.awt.event.ActionListener; 
+import java.awt.event.ActionEvent; 
+import javax.swing.Timer; 
 
-public class BigBang extends JComponent {
+public class BigBang extends JComponent implements ActionListener {
   World world; 
+  Timer timer;
   public BigBang(World world) {
     this.world = world;     
   }
   public void start(int time, JFrame f) { 
     Container c = f.getContentPane();
     c.add(this);   
+    this.timer = new Timer(time, this); 
+    this.timer.start(); 
   } 
   public void paintComponent(Graphics g) {
     this.world.draw(g);     
+  }
+  int count; 
+  public void actionPerformed(ActionEvent e) {
+    System.out.println( this.count++ );     
   }
 }
